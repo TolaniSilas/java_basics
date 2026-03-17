24247946f82ef2c2a95c0947027ad5a2a984fb9e
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-17 06:50:14 -0400

    added the Digit Separator App.

---
+++ java_foundations/DigitSeparatorApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DigitSeparatorApp {
+    public static void main(String[] args) {
+    Scanner inputCollector = new Scanner(System.in);
+
+        System.out.print("Enter a five-digit integer. (For example, 56893) : ");
+        int number = inputCollector.nextInt();
+
+        int firstDigit  = number/ 10000;
+        int secondDigit = number / 1000 % 10;
+        int thirdDigit  = number / 100 % 10;
+        int fourthDigit = number / 10 % 10;
+        int fifthDigit  = number % 10;
+
+        System.out.printf("%d   %d   %d   %d   %d %n", firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit);
+
+        inputCollector.close();
+    }
+
+
+}
