7ac136ed288cd34332f79bcbc972b999aea7d060
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-17 12:11:52 -0400

    added fire drill task 2.

---
+++ fire_drill_two/TaskTwo.java
@@ -0,0 +1,27 @@
+package fire_drill_two;
+import java.util.Scanner;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+
+        System.out.println("A program that collects 10 scores and prints the average.");
+        System.out.println("");
+
+        int i = 0;
+        int sum = 0;
+        while (i++ < 10) {
+            System.out.print("Enter score " + i + ": ");
+            int score = inputCollector.nextInt();
+            sum = sum + score;
+        }
+
+        double average = (double) sum / 10;
+
+        System.out.println("");
+        System.out.println("Average of the scores is: " + average);
+        System.out.println("Done!");
+
+        inputCollector.close();
+    }
+}
