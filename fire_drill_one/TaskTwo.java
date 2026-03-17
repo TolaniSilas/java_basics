ee746d711cc50baefb690daf9b5123bf91f7aabf
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-17 07:43:33 -0400

    added task two and three.

--- fire_drill_one/TaskTwo.java
+++ fire_drill_one/TaskTwo.java
@@ -1,21 +1,26 @@
 package fire_drill_one;
+
 import java.util.Scanner;
 
-public class TaskOne {
+public class TaskTwo {
     public static void main(String[] args) {
         Scanner inputCollector = new Scanner(System.in);
 
-        System.out.println("A program that prints 1 to 10 using loop.");
+        System.out.println("A program that prints even numbers between 1 to 10 using loop.");
         System.out.println("");
         
         System.out.print("Response: " );
         
         int i = 0;
         while (i++ < 10) {
-
+        
+        if (i % 2 == 0) {
         System.out.print(i);
-        System.out.print(" ");
+        
+        
+        }
 
+        System.out.print(" ");
            
         }
         
