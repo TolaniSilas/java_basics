99bfaa15709c18c73939066e49f3cd86b69e7128
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-17 22:39:34 -0400

    added fire drill task 4.

--- fire_drill_two/TaskFour.java
+++ fire_drill_two/TaskFour.java
@@ -1,5 +1,27 @@
 package fire_drill_two;
+import java.util.Scanner;
 
 public class TaskFour {
-    
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+
+        System.out.println("A program that collects 10 scores and prints the sum of even indexes.");
+        System.out.println("");
+
+        int i = 0;
+        int evenSum = 0;
+        while (i++ < 10) {
+            System.out.print("Enter score " + i + ": ");
+            int score = inputCollector.nextInt();
+            if (i % 2 == 0) {
+                evenSum = evenSum + score;
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Sum of even indexes: " + evenSum);
+        System.out.println("Done!");
+
+        inputCollector.close();
+    }
 }
