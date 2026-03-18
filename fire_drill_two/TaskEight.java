1c7b7a420b3ab6596656f822074d8cbaaa5323b9
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-17 23:02:01 -0400

    added fire drill task 8.

--- fire_drill_two/TaskEight.java
+++ fire_drill_two/TaskEight.java
@@ -1,5 +1,32 @@
-package fire_drill_two;
+import java.util.Scanner;
 
 public class TaskEight {
-    
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+
+        System.out.println("A program that collects 10 valid scores between 0 and 100 and prints the sum.");
+        System.out.println("");
+
+        int i = 0;
+        int sum = 0;
+        while (i < 10) {
+            System.out.print("Enter score " + (i + 1) + ": ");
+            int score = inputCollector.nextInt();
+
+            if (score >= 0 && score <= 100) {
+                sum = sum + score;
+                i = i + 1;
+            } 
+            
+            else if (score < 0 || score > 100) {
+                System.out.println("Invalid score entered. Please enter a value between 0 and 100.");
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Sum: " + sum);
+        System.out.println("Done!");
+
+        inputCollector.close();
+    }
 }
