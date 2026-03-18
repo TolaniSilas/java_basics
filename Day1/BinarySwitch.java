4e2db5666406797185f5b06deff1fd75da395ed8
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-18 06:35:41 -0400

    added day 3 files and also refactor day 1 and 2 folders or directories.

---
+++ Day1/BinarySwitch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BinarySwitch {
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+
+        System.out.print("Enter an integer (The number must be 0 or 1): ");
+        int number = inputCollector.nextInt();
+
+        if (number == 0) {
+            System.out.println("The returned number is 1.");
+        }
+
+        if (number == 1) {
+            System.out.println("The returned number is 0.");
+        }
+        
+        if (number != 0) {
+            System.out.println("Enter an integer (The number must be 0 or 1).");
+        }
+
+
+        inputCollector.close();
+    }
+
+
+}
+
---
+++ Day1/Test/DivisibleByThreeApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DivisibleByThreeApp {
+    public static void main(String[] args) {
+    Scanner inputCollector = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = inputCollector.nextInt();
+
+        if (number % 3 == 0) {
+            System.out.println(number + " is divisible by 3.");
+        }
+
+        if (number % 3 != 0) {
+            System.out.println(number + " is not divisible by 3.");
+        }
+
+
+        inputCollector.close();
+    }
+
+
+}
