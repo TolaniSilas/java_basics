168176e9640cd0974015314d8f2145bf0f925e7a
Osunba Silas <tolanisilas3606@gmail.com>
2026-03-18 11:16:59 -0400

    score printer app.

---
+++ Day3/ScorePrinterApp.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ScorePrinterApp {
+    public static void main(String... args) {
+        int[] scores = {13, 22, 32, 14, 35};
+        for(int score : scores){
+            System.out.println(score);
+        }
+        for(int score : scores){
+            System.out.println(score);
+        }
+    } 
+}
